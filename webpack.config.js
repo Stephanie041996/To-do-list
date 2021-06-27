b1fa60c5df0c5a4084bad924631fac74e63ac9ec
Stephanie041996 <michellesakuhuni5@gmail.com>
2021-06-27 08:45:43 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+            'style-loader',
+            'css-loader'
+        ],
+    },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: ['file-loader'],
+      },
+    ],
+  },
+};
